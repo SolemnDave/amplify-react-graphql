cfd692a20ab1d12f392ff13b5c5044cb5ab9a3ea
SolemnDave <95494490+SolemnDave@users.noreply.github.com>
2023-09-25 18:42:54 -0400

    Added graphql and storage

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,13 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://wfaph2n6d5en5jp7zuhezivybu.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-jmn4ry3tmzc7zakttd23guupfa",
+    "aws_user_files_s3_bucket": "howtocontentstorage195447-staging",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
